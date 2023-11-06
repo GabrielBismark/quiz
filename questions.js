c9d0839676e5c3e6459aa5fe4776d55a42a08a06
GabrielBismark <94946017+GabrielBismark@users.noreply.github.com>
2023-11-05 22:19:01 -0300

    bla
    a

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "1- Qual o objetivo do controle de resíduos de pesticidas nos alimentos?",
+      answers: [
+        { option: "avaliar o risco à saúde devido à exposição aos agrotóxicos", correct: true },
+        { option: "avaliar o risco às mudanças de sabores dos alimentos", correct: false },
+        { option: "controlar a temperatura que os alimentos são mantidos para que os mesmos não estraguem", correct: false },
+      ],
+    },
+    {
+      question: "2 Quais são os três princípios ativos microbiológico?",
+      answers: [
+        { option: "agrotóxicos, vermes e micro-organismos", correct: false },
+        { option: " protozoários, algas e moscas", correct: false },
+        { option: "Bactéria, fungos e vírus", correct: true },
+      ],
+    },
+    {
+      question: "3- Qual a diferença entre o uso de pesticidas e o uso de inseticidas?",
+      answers: [
+        { option: "nenhuma, os dois fazem a mesma função", correct: false },
+        { option: "Inseticidas matam insetos e pesticidas controlam pragas", correct: true },
+        { option: "Inseticidas controlam pragas e pesticidas matam insetos", correct: false },
+      ],
+    },
+    {
+      question: "4- De que forma as novas tecnologias, como sensores e drones, ajudam a detectar pragas mais cedo e a reduzir o uso excessivo de pesticidas, como explicado na fala?",
+      answers: [
+        { option: "a tecnologia consegue fazer a aplicação dos defensivos de forma precisa e com maior agilidade", correct: true },
+        { option: "as novas tecnologias criariam pesticidas orgânicos e sintéticos", correct: false },
+        { option: "tecnologias criarão remédios combatentes aos males do uso exagerado de pesticidas", correct: false },
+      ],
+    },
+  ];
